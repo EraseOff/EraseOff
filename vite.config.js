51329e2674507f956af6fc0fc0f83b06a695c1bc
EraseOff <eraseoff@yahoo.com>
2026-09-04 12:33:45 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,29 @@ const { defineConfig } = require('vite');
 const { resolve } = require('node:path');
 
 module.exports = defineConfig({
-  input: {
-    main: resolve(__dirname, 'index.html'),
-    removeGeminiWatermark: resolve(
-      __dirname,
-      'remove-gemini-watermark/index.html'
-    ),
-  },
-
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+
+        removeGeminiWatermark: resolve(
+          __dirname,
+          'remove-gemini-watermark/index.html'
+        ),
+
+        freeGeminiWatermarkRemover: resolve(
+          __dirname,
+          'free-gemini-watermark-remover/index.html'
+        ),
+
+        geminiWatermarkRemoverOnline: resolve(
+          __dirname,
+          'gemini-watermark-remover-online/index.html'
+        ),
+      },
+    },
   },
 });
